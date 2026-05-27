d47bc9ace1fd0122ab09939e86f8b03f8c10a561
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-05-27 16:06:15 +0530

    Learning String

---
+++ StringLengthUserdefine.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//Call by address
+int strlenX(char *ptr)
+{
+    int i = 0;
+
+    while(*ptr != '\0')
+    {
+        i++;
+        ptr++;
+    }
+
+    return i;
+}
+
+int main()
+{
+    char Str[] = "Ganesh";              //100
+    int ret = 0;
+
+    ret = strlenX(Str);                 //strlenX(100)
+
+    printf("String length is :%d\n",ret);
+
+    return 0;
+}
