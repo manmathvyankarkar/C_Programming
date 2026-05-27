d735fc9c6fde950c38739668c43768c900be8180
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-05-27 16:01:43 +0530

    Learning String

---
+++ StringLength.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char Str[] = "Ganesh";
+    int ret = 0;
+
+    ret = strlen(Str);
+
+    printf("String length is :%d\n",ret);
+
+    return 0;
+}
