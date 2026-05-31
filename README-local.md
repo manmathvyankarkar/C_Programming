89dc96330941979a58796fc2e4b7acaf2a1d5b51
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-05-31 21:43:44 +0530

    Learning to create text file

---
+++ README-local.md
@@ -0,0 +1,15 @@
+# First.c
+My first program in C language 
+# C Programming Practice
+
+This repository contains my C programming programs.
+
+## Topics Covered
+- Basics
+- Arrays
+- Pointers
+- Structures
+- Strings
+
+## Author
+Manmath Vyankatkar
---
+++ write_file.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+
+int main()
+{
+    int fd = 0;
+    int Ret = 0;
+    char Arr[] = "Marvellous Infosystems";
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd == -1){
+        printf("Unable to open file");
+    }else{
+        printf("File gets open with fd : %d\n",fd);
+
+        Ret = write(fd,Arr,10);
+
+        printf("%d bytes gets written into the file\n",Ret);
+    }
+
+    //closej(fd);
+
+    return 0;
+}
