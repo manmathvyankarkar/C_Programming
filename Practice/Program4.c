611139d2eafa3fb4b10e27833bf0ae4ecfd1fb30
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-06-06 22:36:33 +0530

    Addition

---
+++ Practice/Program4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+
+    printf("Enter value of i :");
+    scanf("%d", &i);
+
+    printf("Enter value of j :");
+    scanf("%d", &j);
+
+    k = i+j;
+
+    printf("Addition is %d\n",k);
+
+    return 0;5
+}
