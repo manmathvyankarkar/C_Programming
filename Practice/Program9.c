d7b138b95fc303f651e463b644c3ec21e0a20c8b
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-06-06 22:47:33 +0530

    Addition Function

---
+++ Practice/Program9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float AddTwoNumbers (float fNo1, float fNo2)
+{
+    float fAns = 0.0f;          //Variable to store result
+    fAns = fNo1+fNo2;           //Perform Addition
+    return fAns;
+}
+int main()
+{
+    float fValue1 = 0.0f;               //To store first input
+    float fValue2 = 0.0f;               //To store second input
+    float fResult = 0.0f;               //To store the Result
+
+    printf("Enter first no : \n");
+    scanf("%f",&fValue1);
+
+    printf("Enter second no : \n");
+    scanf("%f",&fValue2);
+
+    fResult = AddTwoNumbers(fValue1 , fValue2);            //Perform the addition
+
+    printf("Addition is : %f\n",fResult);
+
+    return 0;
+}
